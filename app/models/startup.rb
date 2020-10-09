50cf848d0ed780b6a61ea04a0d0442487bf389c0
lagofelipe <lagofelipe@gmail.com>
2020-10-09 14:38:03 -0500

    testing pending

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,66 @@
 class Startup
 
-end
+    attr_accessor :name, :domain
+    attr_reader :founder
+
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name = name
+        @domain = domain
+        @founder = founder
+
+        @@all << self
+    end
+
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def find_by_founder(name)
+        self.all.select{ |startup| startup.founder == name }
+    end
+
+
+    def domains
+        self.all.map{ |startup| startup.domain }
+    end
+
+    def sign_contract(venture_capitalist, type, investment)
+      FundingRound.new(self, venture_capitalist, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select{ |funding_round| funding_round.startup == self }
+    end
+
+    def num_funding_rounds
+        funding_rounds.length
+    end
+
+    def total_funds
+        funding_rounds.sum {|o|  o.investment }
+    end
+
+    def investors
+        funding_rounds.compact.uniq
+    end
+
+    def big_investors
+        self.investors.select {|i|i.total_worth > 1000000000}.uniq
+    end
+
+
+
+
+
+
+
+
+######
+end###
--- tools/console.rb
+++ tools/console.rb
@@ -1,11 +1,30 @@
 require_relative '../config/environment.rb'
+require 'pry'
 
 def reload
   load 'config/environment.rb'
 end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+# so they will be available to test and play around with in your console 
+
+s1 = Startup.new("Bob's burgers","Bob", "Bobsburgers.com")
+s2 = Startup.new("Dan's","Danny Cole", "maxauto.com")
+s3 = Startup.new("Heb","Public traded", "heb.com")
+
+vc1  = VentureCapitalist.new("Big mike", 100000)
+vc2  = VentureCapitalist.new("Big steve", 500000)
+vc3  = VentureCapitalist.new("Oscar", 255000)
+vc4  = VentureCapitalist.new("lil mike", 200000)
+
+#startup, venture_capitalist, type, investment
+fr1 = FundingRound.new(s1, vc2, "angel", 500)
+fr2 = FundingRound.new(s3, vc3, "seed", 10000)
+fr3 = FundingRound.new(s2, vc4, "angel", 5000)
+
+puts s2.founder
+# fr2 = s1.sign_contract(vc3, "Series A", 50)
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
